063d30c7257fe0f5a6676f3889880d8e102322c9
Irurnnen <i19v199a56n78@gmail.com>
2025-11-06 01:15:44 +0300

    feat: add validator to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,25 +3,32 @@ package config
 import (
 	"fmt"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
-type Config struct {
-	ServerConfig   *ServerConfig   `mapstructure:"server"`
-	DatabaseConfig *DatabaseConfig `mapstructure:"database"`
-	LogLevel       string          `mapstructure:"log_level"`
-	Debug          bool            `mapstructure:"debug"`
-}
+type (
+	Config struct {
+		ServerConfig   *ServerConfig   `mapstructure:"server" validate:"required"`
+		DatabaseConfig *DatabaseConfig `mapstructure:"database" validate:"required"`
+		LogLevel       string          `mapstructure:"log_level" validate:"omitempty"`
+	}
 
-type DatabaseConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	User     string `mapstructure:"user"`
-	Password string `mapstructure:"password"`
-	DBName   string `mapstructure:"db_name"`
-	Secure   bool   `mapstructure:"secure"`
-}
+	DatabaseConfig struct {
+		Host     string `mapstructure:"host" validate:"required,hostname"`
+		Port     int    `mapstructure:"port" validate:"required,port"`
+		User     string `mapstructure:"user" validate:"required"`     // TODO: add custom validator
+		Password string `mapstructure:"password" validate:"required"` // TODO: add custom validator
+		DBName   string `mapstructure:"db_name" validate:"required"`
+		Secure   bool   `mapstructure:"secure" validate:"omitempty"`
+	}
+
+	ServerConfig struct {
+		Host string `mapstructure:"host" validate:"required,hostname"`
+		Port int    `mapstructure:"port" validate:"required,port"`
+	}
+)
 
 func (d *DatabaseConfig) GetDSN() string {
 	DSN := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", d.User, d.Password, d.Host, d.Port, d.DBName)
@@ -29,11 +36,6 @@ func (d *DatabaseConfig) GetDSN() string {
 		return DSN
 	}
 	return DSN + "?sslmode=disable"
-}
-
-type ServerConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
 }
 
 func NewConfig() *Config {
@@ -46,11 +48,17 @@ func NewConfig() *Config {
 		return nil
 	}
 
-	var config Config
+	config := new(Config)
 	if err := viper.Unmarshal(&config); err != nil {
 		log.Fatal().Err(err).Msg("Failed to parse config")
 		return nil
 	}
 
-	return &config
+	validate := validator.New()
+	if err := validate.Struct(config); err != nil {
+		log.Fatal().Err(err).Msg("Failed to validate config")
+		return nil
+	}
+
+	return config
 }
